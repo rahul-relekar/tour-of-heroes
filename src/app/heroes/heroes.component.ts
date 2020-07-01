4af1f1e1d72439be3b4464c5ea2fe54129020790
DESKTOP-JBBB9I8\Rahul Relekar <rrelekar2-c@my.cityu.edu.hk>
2020-07-01 19:04:59 +0800

    Complete display of selection list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,17 +7,18 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
-  hero: Hero={
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
   constructor() { }
 
-  
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
