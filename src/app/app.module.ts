5d4ca4d8c125e4efb609f12b9d1b05b43338e635
DESKTOP-JBBB9I8\Rahul Relekar <rrelekar2-c@my.cityu.edu.hk>
2020-07-01 20:06:04 +0800

    1. Created a separate, reusable HeroDetailComponent. 2. Used a property binding to give the parent HeroesComponent control over the child HeroDetailComponent. 3. Used the @Input decorator to make the hero property available for binding by the external HeroesComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
